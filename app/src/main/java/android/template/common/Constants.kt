330a05f33b853382f84c27f1973b39e48c83d26f
theo-beep <56303873+theo-beep@users.noreply.github.com>
2024-05-12 08:16:26 +0200

    - Adding constants - Adding DataResources

---
+++ app/src/main/java/android/template/common/Constants.kt
@@ -0,0 +1,9 @@
+package android.template.common
+
+object Constants {
+    const val BASE_URL = "https://api.open-meteo.com/"
+    const val DEGREE_CELSIUS = "\u2103"
+    const val PRESSURE_UNIT = "hpa"
+    const val HUMIDITY_UNIT = "%"
+    const val WIND_UNIT = "km/h"
+}
