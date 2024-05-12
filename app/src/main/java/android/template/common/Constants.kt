5b32176dc5775e9c06e81d6937ccf9dad857af52
theo-beep <56303873+theo-beep@users.noreply.github.com>
2024-05-12 08:20:15 +0200

    - fix base url and remove unused Constants

--- app/src/main/java/android/template/common/Constants.kt
+++ app/src/main/java/android/template/common/Constants.kt
@@ -1,9 +1,5 @@
 package android.template.common
 
 object Constants {
-    const val BASE_URL = "https://api.open-meteo.com/"
-    const val DEGREE_CELSIUS = "\u2103"
-    const val PRESSURE_UNIT = "hpa"
-    const val HUMIDITY_UNIT = "%"
-    const val WIND_UNIT = "km/h"
+    const val BASE_URL = "https://api-jrdvg5xikq-ew.a.run.app"
 }
